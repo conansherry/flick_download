546e22997fd8ec3ce9d73b8b9efb34d2bbcf6e72
conansherry <conansherry@163.com>
2017-01-07 15:58:39 +0800

    add tags

--- mydownload.py
+++ mydownload.py
@@ -3,10 +3,12 @@ import urllib, urlparse
 import os
 import sys
     
-tag_list = []
+tag_list = ['Dance', 'Sing', 'Eat', 'Food', 'Play', 'People', 'Family', 'Dog', 'Cat', 'Animals', 'Car', 'Man', 'Woman', 'Baby', 'Bird', 'Lake', 'Forest', 'Sunshine', 'City', 'Street', 'Building']
 page_index = ['1', '2', '3', '4', '5', '6']
 
 for tag in tag_list:
+    if not os.path.isdir(tag):
+        os.mkdir(tag)
     for pi in page_index:
         # downloading image data
         f = flickr.photos_search(tags=tag, per_page='1', page=pi)
@@ -21,5 +23,5 @@ for tag in tag_list:
                 continue
             urllist.append(url)
             image = urllib.URLopener()
-            image.retrieve(url, os.path.basename(urlparse.urlparse(url).path))
+            image.retrieve(url, os.path.join(tag, os.path.basename(urlparse.urlparse(url).path)))
             print 'downloading:', url
